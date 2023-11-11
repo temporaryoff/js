53432968b7b590b56964671a43df307f57f50f7b
ybp345 <ybp345@gmail.com>
2023-11-12 00:31:52 +0530

    datatypes complete

---
+++ datatypes.js
@@ -0,0 +1,11 @@
+"use strict" // treat all js as newer version
+
+// alert(3 + 3);  //we are using nodejs, not browser
+
+console.log( 3 + 3);
+console.log("Yogesh");
+let name = "Patil";
+let age = 18;
+let isLoogedIn = false;
+
+// number => 2 to the power of 53
