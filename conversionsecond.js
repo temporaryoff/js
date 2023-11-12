ef8a85387d33fd4876d5da54ba98a1e19c88f6a5
ybp345 <ybp345@gmail.com>
2023-11-12 15:12:02 +0530

    07 complete

---
+++ conversionsecond.js
@@ -0,0 +1,32 @@
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello";
+let str2 = " Yogesh";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("2" + 5);
+console.log(5 + "2");
+console.log("1" + 2 + 2);
+console.log( 5 + 2 + "2");
+
+console.log(true);
+console.log(-true);
+console.log(+true);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
