254fb569ee6f7b26886a2e47e5ba4b910a8798ae
ybp345 <ybp345@gmail.com>
2023-11-19 12:57:13 +0530

    js memory execution

---
+++ how_does_js_execute_code.js
@@ -0,0 +1,5 @@
+// 1) browser ani node, deno, ya envirnment madhe GLOBAL EXECUTION CONTEXT ha vegla asto ani browser madhe vegla asto.  borwser madhe this chi value winwod object yete. api process chaya veli ya this ANI GLOABL ECECUTION CONTEXT  cha use hoto tya shivay react cha code run hot nahi.
+// 2) javascript he ek single threaded language aahe.
+// 3) 2nd veli FUNCTION EXECUTION CONTEXT RUN HOT ASTO.
+// 4) Javascript run hotana 1st a) memory creation phase tayar hot asto.  2) 2nd phase hee executiion hot aste.
+// 5) check google doc js chai aur code4=
