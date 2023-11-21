a19fdd484e189181ebd273a00a9325c4ddc1d4e4
ybp345 <ybp345@gmail.com>
2023-11-22 04:52:13 +0530

    switch complete

---
+++ switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+            console.log("Feb");
+            break;
+    case "march":
+            console.log("March");
+            break;
+    case "apr":
+            console.log("April");
+            break;
+
+    default:
+        console.log("Default case Match");
+        break;
+}
